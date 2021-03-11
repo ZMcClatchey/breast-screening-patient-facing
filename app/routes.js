07c21f88ed78a393398a84bca98a46c1e4fe6c1e
monsterlimo <mattnixon79@googlemal.com>
2021-03-11 15:59:55 +0000

    fixed the arrays

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,46 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.post("/store-contact-preferences/", function (req, res) {
+    const adj = req.session.data['contact-preferences'];
+    let adjList = [];
+    if (Array.isArray(adj)) {
+        adj.forEach((ad) => {
+            adjList.push(ad);
+        });
+    } else {
+        adjList.push(adj)
+    }
+    req.session.data['contact-preferences'] = adjList;
+    res.redirect('/preferences/review-preferences');
+})
+
+router.post("/store-adjustments/", function (req, res) {
+    const adj = req.session.data['adjustments'];
+    let adjList = [];
+    if (Array.isArray(adj)) {
+        adj.forEach((ad) => {
+            adjList.push(ad);
+        });
+    } else {
+        adjList.push(adj)
+    }
+    req.session.data['adjustments'] = adjList;
+    res.redirect('/preferences/appointment-access-needs');
+})
+
+router.post("/store-needs/", function (req, res) {
+    const adj = req.session.data['access-needs'];
+    let adjList = [];
+    if (Array.isArray(adj)) {
+        adj.forEach((ad) => {
+            adjList.push(ad);
+        });
+    } else {
+        adjList.push(adj)
+    }
+    req.session.data['access-needs'] = adjList;
+    res.redirect('/preferences/review-preferences');
+})
+
 module.exports = router;
